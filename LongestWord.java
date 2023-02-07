8709b033c80b595e0a8b5fb1bf3c1cec984f2d8b
Andrew <123314186+AndrewKorotkov@users.noreply.github.com>
2023-02-07 13:37:35 +0300

    LongestWord

--- LongestWord.java
+++ LongestWord.java
@@ -1,21 +1,18 @@
 package HomeworkTwo;
 
 public class LongestWord {
-    static String max(String par)
-    {
-        String[]words=par.split(" ");
-        String resstring="";
-        for(String word: words)
-        {
-            if(word.length()>resstring.length())
-                resstring=word;
+    static String max(String par) {
+        String[] words = par.split(" ");
+        String resstring = "";
+        for (String word : words) {
+            if (word.length() > resstring.length())
+                resstring = word;
         }
-        return(resstring);
+        return (resstring);
     }
 
-    static public void main(String args[])
-    {
-        String somestring="one two three four five";
+    static public void main(String args[]) {
+        String somestring = "one two three four five";
         String maxword = max(somestring);
         System.out.println(maxword);
         System.out.println(maxword.length());
